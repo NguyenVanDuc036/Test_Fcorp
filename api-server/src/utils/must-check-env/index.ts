01f6144ace1df1acb39bb43a6767a33f10ae182f
nguyenvanduc036 <vanduccntt036@gmail.com>
2023-07-15 17:57:57 +0700

    update docs

--- api-server/src/utils/must-check-env/index.ts
+++ api-server/src/utils/must-check-env/index.ts
@@ -1,14 +1,14 @@
 export const mustCheckEnv = (...names: string[]) => {
-  const empty = names.filter((k) => !process.env[k]);
+  const empty = names.filter((key) => !process.env[key]);
   if (empty.length) {
     throw new Error(`Missing env ${empty.join(", ")}`);
   }
 };
 
 export const mustCheckEnvValues = (name: string, values: string[]) => {
-  const v = process.env[name] as string
-  if (!values.includes(v)) {
-    throw new Error(`Invalid env ${name}=${v} expect ${values.join(' | ')}`)
+  const env= process.env[name] as string
+  if (!values.includes(env)) {
+    throw new Error(`Invalid env ${name}=${env} expect ${values.join(' | ')}`)
   }
 }
 
